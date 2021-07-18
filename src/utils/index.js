32fed231ddf679d8a60153d2c2f3dd94bf70e851
Timothy Merfry Tiwow <s21810014@student.unklab.ac.id>
2021-07-18 19:09:48 +0800

    added helper functions

---
+++ src/utils/index.js
@@ -0,0 +1,33 @@
+module.exports = {
+    checkFields: (obj1, keysList) => {
+        let foreignKeys = []
+        let count = 0
+        let err
+
+        for(const key in obj1) {
+            if(!keysList.includes(key))
+                foreignKeys.push(key)
+            count++
+        }
+
+        if (count < keysList.length)
+            err = { result: `Form Incomplete, required fields [${keysList}]` }
+        else if (foreignKeys.length > 0)
+            err = { result: `Please do not specify '${foreignKeys[0]}'`}
+
+        return err
+    },
+
+    errorFormatter: (error) => {
+        switch(error.name) {
+            case "SequelizeUniqueConstraintError": {
+                return {
+                    result: 'Object already exist'
+                }
+            }
+            default: {
+                return error.errors[0]
+            }
+        }
+    }
+}
